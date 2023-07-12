dc12ec8017d243ba8c25ee0bb95dee494fe78e8e
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-07-11 20:02:35 -0400

    upload image to s3

---
+++ avatars/provider.py
@@ -0,0 +1,26 @@
+import boto3
+from botocore.exceptions import ClientError
+# Environment variables
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+class AWSProvider:
+    def upload_file_s3(self, path_to_save, path_to_file, bucket=env('BUCKET_NAME')):
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=env('ACCESS_KEY'),
+            aws_secret_access_key=env('SECRET_ACCESS_KEY')
+        )
+        try:
+            s3_client.upload_file(path_to_file, bucket, Key=path_to_save)
+            url = s3_client.generate_presigned_url(
+                'get_object',
+                ExpiresIn=0,
+                Params={'Bucket': bucket, 'Key': path_to_save}
+            )
+            return str(url).split('?')[0]
+        except ClientError as error:
+            print(error)
+            return False
--- avatars/views.py
+++ avatars/views.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+import os
+
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 
 from avatars.models import AvatarModel
+from avatars.provider import AWSProvider
 from avatars.serializers import AvatarSerializer
 
+awsProvider = AWSProvider()
 
 class AvatarViewSet(viewsets.ModelViewSet):
     queryset = AvatarModel.objects.all()
@@ -13,8 +19,38 @@ class AvatarViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["user_id"]
 
-    # def create(self, request):
-    #     try:
-    #         # upload avatar to aws s3     
-    #     except:
-    #         return 
+    def create(self, request):
+        try:
+            if not "file" in request.data:
+                return Response({"file": ["a file must be informed."]}, status=400)
+
+            if not "user_id" in request.data:
+                return Response({"user": ["a user must be informed"]}, status=400)
+
+            file = request.data["file"]
+            user = self.request.user
+            print(user)
+
+            photo_path = f'./files/avatar-{datetime.now().strftime("%H%M%S-%F")}.png'
+            with open(photo_path, "wb+") as archive:
+                archive.write(file.file.read())
+
+            url_photo = awsProvider.upload_file_s3(
+                f"users-avatar/{user.id}.png", photo_path
+            )
+            os.remove(photo_path)
+
+            data = {
+                "user_id": request.data["user_id"],
+                "file": request.data["file"],
+                "url": url_photo,
+            }
+            serializer = AvatarSerializer(data=data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=201)
+
+            return Response(serializer.errors, status=400)
+        except Exception as e:
+            return Response(f"Failure to process avatar data: {e}", status=500)
