27955ad4959297dd3e89874b7e9bc0e5643e8751
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-07-13 08:56:14 -0400

    password hash

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from users.models import User
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+    def validate_password(self, value: str) -> str:
+        """
+        Hash value passed by user.
+
+        :param value: password of a user
+        :return: a hashed version of the password
+        """
+        return make_password(value)
